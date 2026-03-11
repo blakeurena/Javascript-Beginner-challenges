71ba4dafc9cc7d4d8f4e184dbb67a398dff0fb99
blakeurena <blakeurenact@gmail.com>
2026-03-10 20:55:47 -0500

    Add app.js with various functions

---
+++ app.js
@@ -0,0 +1,107 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addition(3, 5));
+
+function hoursIntoSeconds(hour) {
+   let minutes = hour * 60;
+    let seconds = minutes * 60;
+    return seconds;
+    }
+
+console.log(hoursIntoSeconds(2));
+
+function calcPerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+console.log(calcPerimeter(10, 20));
+
+function calcAreaofTriangle(base, height) {
+    return 1 / 2 * base * height;
+}
+
+console.log(calcAreaofTriangle(20, 20));
+
+function appendFrontend(frntnd) {
+    
+    return 4 + number
+}
+
+console.log(appendFrontend(1));
+
+function GreaterThan100(num1, num2) {
+    if (num1 + num2 >= 100) {
+        return true;
+    }
+        return false;
+    }
+
+
+console.log(GreaterThan100(50, 50));
+
+function lessThanOrEqualToZero(num) {
+    if (num <= 0) {
+        return true;
+    }
+        return false;
+    }
+
+console.log(lessThanOrEqualToZero(-5));
+
+function oppositeBoolean(bool) {
+    return !bool;
+}
+
+console.log(oppositeBoolean(false));
+
+function isNotZero(num) {
+    return num !== 0
+}
+
+console.log(isNotZero(5));
+
+function calcRemainder(num1, num2) {
+    return num1 % num2;
+}
+
+console.log(calcRemainder(10, 3));
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(4));
+
+function isOdd(num) {
+    return num % 2 !== 0;
+}
+
+console.log(isOdd(5));
+
+function booleaninterger(num) {
+    return num > 0 ? 1 : -1;
+}
+
+console.log(booleaninterger(15));
+
+function isLoggedInAndSubscribed(loggedIn, subscribed) {
+    return loggedIn && subscribed;
+}
+
+console.log(isLoggedInAndSubscribed(true, true));
+
+function isLoggedInOrSubscribed(loggedIn, subscribed) {
+    return loggedIn || subscribed;
+}
+
+console.log(isLoggedInOrSubscribed(true, false));
+
+function isLoggedInAndUnsubscribed(loggedIn, unsubscribed) {
+    return loggedIn && unsubscribed;
+}
+
+console.log(isLoggedInAndUnsubscribed(true, false));
+
+
